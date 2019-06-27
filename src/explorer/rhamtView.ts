f47bc899e27e6d574d201b841e671f82382a2f43
John Steele <johnsteele@redhat.com>
2019-06-27 15:58:34 -0400

    Remove unused disposable.

--- src/explorer/rhamtView.ts
+++ src/explorer/rhamtView.ts
@@ -6,18 +6,14 @@ import * as vscode from 'vscode';
 import { RhamtExplorer } from './rhamtExplorer';
 import { ModelService } from '../model/modelService';
 
-export class RhamtView implements vscode.Disposable {
+export class RhamtView {
 
     constructor(private context: vscode.ExtensionContext,
         private modelService: ModelService) {
         this.createExplorer();
-        this.context.subscriptions.push(this);
     }
 
     private createExplorer(): RhamtExplorer {
         return new RhamtExplorer(this.context, this.modelService);
-    }
-
-    public dispose(): void {
     }
 }
