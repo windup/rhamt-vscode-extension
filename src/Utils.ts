b0fe8ce77165afd7661978da2cb205c5105fb317
John Steele <programjsteele@gmail.com>
2019-12-11 16:37:55 -0500

    update to newer version of rhamt

--- src/Utils.ts
+++ src/Utils.ts
@@ -14,10 +14,10 @@ const RHAMT_VERSION_REGEX = /^version /;
 
 const findJava = require('find-java-home');
 
-const RHAMT_VERSION = '4.3.0.Beta1-offline';
+const RHAMT_VERSION = '4.3.0.Final';
 const RHAMT_FOLDER = `rhamt-cli-${RHAMT_VERSION}`;
 // const DOWNLOAD_CLI_LOCATION = `http://central.maven.org/maven2/org/jboss/windup/rhamt-cli/${RHAMT_VERSION}/${RHAMT_FOLDER}-offline.zip`;
-const PREVIEW_DOWNLOAD_CLI_LOCATION = 'https://github.com/johnsteele/windup/releases/download/v0.0.2-alpha/rhamt-cli-4.3.0.Beta1-offline.zip';
+const PREVIEW_DOWNLOAD_CLI_LOCATION = 'https://github.com/johnsteele/windup/releases/download/v0.0.3-alpha/rhamt-cli-4.3.0.Final-offline.zip';
 const IGNORE_RHAMT_DOWNLOAD = 'ignoreRhamtDownload';
 
 export namespace Utils {
