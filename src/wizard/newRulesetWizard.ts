6be157377c7e416efecf59400af55ffa00af8f16
John Steele <programjsteele@gmail.com>
2020-01-06 18:15:43 -0500

    Add default implementation to the XML ruleset wizard

--- src/wizard/newRulesetWizard.ts
+++ src/wizard/newRulesetWizard.ts
@@ -1,7 +1,21 @@
 
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export class NewRulesetWizard {
 
-    open () {
+    async open (): Promise<any> {
+        const fileName = await vscode.window.showInputBox({value: 'custom-ruleset.rhamt.xml', placeHolder: 'File name', valueSelection: [0, 14]});
+        if (vscode.workspace.workspaceFolders) {
+            const root = vscode.workspace.workspaceFolders[0].uri.fsPath;
+            const location = path.join(root, fileName);
+            return new Promise((resolve, reject) => {
+                fs.writeFile(location, '', null, e => {
+                    if (e) reject(e);
+                    else resolve();
+                });
+            })
+        }
     }
 }
