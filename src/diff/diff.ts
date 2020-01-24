74ade49b60069f9a3af7b4bd48e4cac557c68583
John Steele <programjsteele@gmail.com>
2020-01-24 15:41:58 -0500

    WINDUP-2541 - Starting point for previewing quickfixes of type REPLACE.

--- src/diff/diff.ts
+++ src/diff/diff.ts
@@ -3,14 +3,40 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { IIssue } from "../model/model";
+import { IHint, IQuickFix } from "../model/model";
+import { createRandomFile } from './utils';
+import * as fs from 'fs';
 import * as vscode from 'vscode';
 
 export class Diff {
 
-    static compare(issue: IIssue): void {
+    static async compare(hint: IHint, quickfix: IQuickFix): Promise<any> {
+        try {
+            const file = vscode.Uri.parse(hint.file);
+            var content = fs.readFileSync(file.fsPath, 'utf8');
+            const tmp = await createRandomFile(content);
+            const written = await Diff.writeTemp(tmp, quickfix, hint);
+            if (!written) {
+                throw new Error('Unable to write quickfix file.');
+            }
+            await Diff.openDiff(file, tmp);
+        }
+        catch (e) {
+            const msg = `Quickfix Error - ${e}`;
+            console.log(msg);
+            vscode.window.showErrorMessage(msg);
+        }        
     }
 
-    static async openDiff(original: vscode.Uri, modified: string): Promise<any> {
+    static writeTemp(file: vscode.Uri, quickfix: IQuickFix, hint: IHint): Thenable<boolean> {
+        const edit = new vscode.WorkspaceEdit();
+        if (quickfix.type === 'REPLACE') {
+            edit.insert(file, new vscode.Position(hint.lineNumber, hint.column), quickfix.replacementString);
+            return vscode.workspace.applyEdit(edit);
+        }
+    }
+
+    static async openDiff(original: vscode.Uri, modified: vscode.Uri): Promise<any> {
+        return vscode.commands.executeCommand('vscode.diff', original, modified, 'Quickfix Preview', {preview: true});
     }
 }
