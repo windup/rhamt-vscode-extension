84d4e1f80feef57fe79d04724990dc0d6dd38d03
John Steele <programjsteele@gmail.com>
2020-03-19 15:25:36 -0400

    WINDUP-2607 - read and render quickfix preveiew editor content

--- src/quickfix/contentProvider.ts
+++ src/quickfix/contentProvider.ts
@@ -1,8 +1,18 @@
 import * as vscode from "vscode";
+import { ModelService } from "../model/modelService";
+import * as fs from 'fs';
 
 export class QuickfixContentProvider implements vscode.TextDocumentContentProvider {
+
+    constructor(private modelService: ModelService) {
+    }
     
-    provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): string {
-        return '';
+    async provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): Promise<string> { 
+        const config = this.modelService.getConfiguration(uri.authority);
+        const hints = await config.results.getHints();
+        const hint = hints.find(hint => hint.id === uri.path.substring(1));
+        let file = vscode.Uri.file(hint.file);
+        var content = fs.readFileSync(file.fsPath, 'utf8');
+        return content;
     }
 }
