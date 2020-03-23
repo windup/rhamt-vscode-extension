81e926458b7c01ef0a2ff132c981b88afd8dc086
John Steele <programjsteele@gmail.com>
2020-03-23 18:28:14 -0400

    WINDUP-2607 - added virutal file support to REPLACE type quickfixes.

--- src/quickfix/quickfix.ts
+++ src/quickfix/quickfix.ts
@@ -2,21 +2,25 @@
  *  Copyright (c) Red Hat. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
 import * as vscode from 'vscode';
 import { IQuickFix } from '../model/model';
+import * as fs from 'fs-extra';
 
-export function applyReplaceQuickfix(quickfix: IQuickFix): Promise<any> {
-    return new Promise<any>(resolve => {
-        vscode.workspace.openTextDocument(vscode.Uri.file(quickfix.file)).then(async doc => {
-            const editor = await vscode.window.showTextDocument(doc);
-            editor.edit(editBuilder => {
-                const all = new vscode.Range(
-                    doc.positionAt(0),
-                    doc.positionAt(doc.getText().length)
-                );
-                editor.edit(eb => eb.replace(all, '')).then(resolve);
-            });
-        });
-    });
+export async function applyReplaceQuickfix(quickfix: IQuickFix): Promise<void> {
+    const issue = quickfix.issue as any;
+    const config = issue.configuration;
+    if (quickfix.type === 'REPLACE' && issue.lineNumber) {
+        const file = vscode.Uri.parse(`quickfixed://${config.id}/${quickfix.issue.id}?${quickfix.id}`);
+        const doc = await vscode.workspace.openTextDocument(file);
+        let edit = new vscode.WorkspaceEdit();
+        const lineNumber = issue.lineNumber-1;
+        const end = doc.lineAt(lineNumber).range.end;
+        edit.delete(file, new vscode.Range(lineNumber, 0, lineNumber, end.character));
+        await vscode.workspace.applyEdit(edit);
+        edit = new vscode.WorkspaceEdit();
+        const replacement = issue.quickfixedLines[quickfix.id];
+        edit.replace(file, new vscode.Range(lineNumber, 0, lineNumber, replacement.length), replacement);
+        await vscode.workspace.applyEdit(edit);
+        fs.writeFileSync(issue.file, doc.getText());
+    }
 }
