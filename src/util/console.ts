a16f1dd91661b0630e794f53fdf20af19a546566
John Steele <programjsteele@gmail.com>
2020-04-21 13:28:02 -0400

    Add EOL and License to console.

--- src/util/console.ts
+++ src/util/console.ts
@@ -1,4 +1,7 @@
-
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import * as vscode from 'vscode';
 
 class RhamtChannelImpl {
