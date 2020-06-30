c4a484a8407305e07c5b71e395ad2d80f65c02ee
John Steele <programjsteele@gmail.com>
2020-06-30 17:03:49 -0400

    add snapshot to release path

--- src/Utils.ts
+++ src/Utils.ts
@@ -16,7 +16,7 @@ const RHAMT_VERSION_REGEX = /^version /;
 
 const findJava = require('find-java-home');
 
-const PREVIEW_DOWNLOAD_CLI_LOCATION = 'https://github.com/johnsteele/windup/releases/download/v4.3.2-SNAPSHOT/mta-cli-4.3.2-offline.zip';
+const PREVIEW_DOWNLOAD_CLI_LOCATION = 'https://github.com/johnsteele/windup/releases/download/v4.3.2-SNAPSHOT/mta-cli-4.3.2-SNAPSHOT-offline.zip';
 // const PREVIEW_DOWNLOAD_CLI_LOCATION = 'https://oss.sonatype.org/content/repositories/snapshots/org/jboss/windup/rhamt-cli/4.3.2-SNAPSHOT/rhamt-cli-4.3.2-20200511.064139-57-offline.zip';
 // const DOWNLOAD_CLI_LOCATION = `http://central.maven.org/maven2/org/jboss/windup/rhamt-cli/${RHAMT_VERSION}/${RHAMT_FOLDER}-offline.zip`;
 
