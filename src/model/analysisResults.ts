c7ab1a5e05bf1616963dd00249845a0d8c520be4
John Steele <programjsteele@gmail.com>
2021-10-28 10:45:22 -0700

    Parse any file type for computing quickfix info.

--- src/model/analysisResults.ts
+++ src/model/analysisResults.ts
@@ -7,7 +7,7 @@ import * as cheerio from 'cheerio';
 import { ModelService } from './modelService';
 import * as open from 'opn';
 import * as readline from 'readline';
-import * as mime from 'mime-types';
+// import * as mime from 'mime-types';
 import { IHint, IQuickFix, IClassification, RhamtConfiguration, IIssue, ILink, IIssueType } from './model';
 
 export interface AnalysisResultsSummary {
@@ -376,11 +376,17 @@ export class AnalysisResults {
         // Otherwise, read the quickfix data to be serialized later on
         if ((issue as any).lineNumber) {
             const hint = issue as IHint;
-            const type = mime.lookup(hint.file);
+            // const type = mime.lookup(hint.file);
             // TODO: This will ignore .properties files and others that we might want to support.
-            if (type && type.startsWith('text')) {
+            // if (type && type.startsWith('text')) {
                 if (!hint.originalLineSource) {
-                    hint.originalLineSource = await this.readLine(hint.file, hint.lineNumber);
+                    hint.originalLineSource = 'unable to parse file';
+                    try {
+                        hint.originalLineSource = await this.readLine(hint.file, hint.lineNumber);
+                    }
+                    catch (e) {
+                        console.log('error reading file to compute quickfix info: ' + hint.file);
+                    }
                 }
                 if (quickfix.type === 'REPLACE') {
                     const quickfixedLine = this.replace(
@@ -389,10 +395,10 @@ export class AnalysisResults {
                         quickfix.replacementString);
                     hint.quickfixedLines[quickfix.id] = quickfixedLine;
                 }
-            }
-            else {
-                console.log(`unable to read mime type ${type} of file - ${hint.file}`);
-            }
+            // }
+            // else {
+            //     console.log(`unable to read mime type ${type} of file - ${hint.file}`);
+            // }
         }
         return quickfix;
     }
