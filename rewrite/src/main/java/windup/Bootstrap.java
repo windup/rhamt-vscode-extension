52ffd6ed162c12831fa656d3083283f9927e2b28
John Steele <programjsteele@gmail.com>
2022-04-04 20:22:45 -0700

    start rewrite integration

---
+++ rewrite/src/main/java/windup/Bootstrap.java
@@ -0,0 +1,13 @@
+package windup;
+
+public class Bootstrap {
+
+    public static void main(String[] args) {
+    }
+
+    private static void runRewriteWith(String[] args) {
+    }
+
+    private static void startServer(String[] args) {
+    }
+}
---
+++ rewrite/src/test/java/windup/AppTest.java
@@ -0,0 +1,14 @@
+package windup;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
