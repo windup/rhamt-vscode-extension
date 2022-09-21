80c1d83f9cbd15eed32841b5b54648579cdcf623
John Steele <programjsteele@gmail.com>
2022-09-20 17:39:49 -0700

    Fix README publisher format. Rename explorer to more generic title.

--- src/source/markers.ts
+++ src/source/markers.ts
@@ -7,7 +7,7 @@ import { window, ThemeColor } from 'vscode';
 import { IHint } from '../model/model';
 import { ModelService } from '../model/modelService';
 
-export const HINT = 'MTR';
+export const HINT = 'migration';
 
 export class MarkerService {
 
